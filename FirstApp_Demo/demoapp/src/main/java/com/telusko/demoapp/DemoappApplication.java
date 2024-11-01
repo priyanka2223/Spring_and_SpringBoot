941f83d47b747b4ec6c58d8459e4a1c7870b324a
priyanka2223 <39003754+priyanka2223@users.noreply.github.com>
2024-11-01 22:10:02 +0530

    Add files via upload
    First Spring Boot Web App

---
+++ FirstApp_Demo/demoapp/src/main/java/com/telusko/demoapp/DemoappApplication.java
@@ -0,0 +1,14 @@
+package com.telusko.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoappApplication.class, args);
+	}
+
+}
